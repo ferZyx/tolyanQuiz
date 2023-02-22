856972d1ab50851377f75736085890feb84f141a
ferZyx <sorokin.wlad@yandex.ru>
2023-02-22 17:03:14 +0600

    static files fix6

--- mainQuizApp/urls.py
+++ mainQuizApp/urls.py
@@ -16,4 +16,4 @@ urlpatterns = [
     path('testing/<test_id>', login_required(testing_page), name='testing_page'),
 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
